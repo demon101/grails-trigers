c14923ddcbdbad9ba702e18adffc01080adca127
dgalinskii <d.galinskii@avax.de>
2017-04-07 16:22:17 +0200

    trigers variables scopes

--- grails-app/domain/grails/trigers/SomeDomain.groovy
+++ grails-app/domain/grails/trigers/SomeDomain.groovy
@@ -7,7 +7,6 @@ class SomeDomain {
 
     static constraints = {
         engName nullable: false, blank:false
-
         linkName unique: true, nullable: false, blank:false
     }
 
@@ -17,33 +16,21 @@ class SomeDomain {
 
     def beforeValidate(){
         log.error "beforeValidate(id=${id})"
-        engName = engName?.trim()//?.decodeHTML()
 
         if (!id || isDirty('engName')){
             def oldLinkName = linkName
             linkName = engName.toLowerCase() // some dummy transformation
-            if (true){
-                if (SomeDomain.countByLinkNameAndIdNotEqual(linkName, id?:0L)){
-                    linkName = "$engName ${1}".toLowerCase()
-                    log.error("linkName '${oldLinkName}' already exist, new link name is ${linkName}")
-                }
+            if (SomeDomain.countByLinkNameAndIdNotEqual(linkName, id?:0L)){ //causes beforeUpdate !!!
+                linkName = "$engName ${1}".toLowerCase()
+                log.error("linkName '${oldLinkName}' already exist, new link name is ${linkName}")
             }
         }
 
-//        if (isDirty('matureApproved') && SpringSecurityUtils.ifNotGranted(UserRole.CAN_APPROVE_MATURE)){
-//            matureApproved = getPersistentValue('matureApproved')
-//        }
-//
-//        if (matureApproved && isDirty('mature') && SpringSecurityUtils.ifNotGranted(UserRole.CAN_APPROVE_MATURE)){
-//            mature = getPersistentValue('mature')
-//        }
         log.error "beforeValidate end(id=${id})"
-
     }
 
     def beforeInsert () {
         OldLink.findByOldAndTypeId(linkName, null)?.delete()
-        //must return true to save!
         return true
     }
 
@@ -53,7 +40,7 @@ class SomeDomain {
         if (isDirty('linkName')){
             def oldLink = getPersistentValue('linkName')
             if (oldLink != linkName){
-                log.error "Create manga OldLink $oldLink -> $linkName"
+                log.error "Create OldLink $oldLink -> $linkName"
                 def l = new OldLink(old: oldLink, current: linkName)
                 if (!l.save()) log.error l.errors
             }
